8d6d441fb937ee72d6d631ac0df409bc90b8b366
tebteban <tebancejas@gmail.com>
2026-03-18 02:06:05 -0300

    cloudinary otra vez

--- src/controllers/adminPagesController.js
+++ src/controllers/adminPagesController.js
@@ -1325,9 +1325,18 @@ controller.storeOrgano = async (req, res) => {
     try {
         const data = { name: req.body.name, description: req.body.description, color: req.body.color, topic: req.body.topic };
         if (req.files) {
-            if (req.files['reglamento']?.[0])    data.link_reglamento = '/uploads/documents/' + req.files['reglamento'][0].filename;
-            if (req.files['archivo_dinamicas']?.[0]) data.link_dinamicas = '/uploads/documents/' + req.files['archivo_dinamicas'][0].filename;
-            if (req.files['archivo_topico']?.[0])    data.link_topico    = '/uploads/documents/' + req.files['archivo_topico'][0].filename;
+            if (req.files['reglamento']?.[0]) {
+                const f = req.files['reglamento'][0];
+                data.link_reglamento = process.env.NODE_ENV === 'production' && f.path ? f.path : '/uploads/documents/' + f.filename;
+            }
+            if (req.files['archivo_dinamicas']?.[0]) {
+                const f = req.files['archivo_dinamicas'][0];
+                data.link_dinamicas = process.env.NODE_ENV === 'production' && f.path ? f.path : '/uploads/documents/' + f.filename;
+            }
+            if (req.files['archivo_topico']?.[0]) {
+                const f = req.files['archivo_topico'][0];
+                data.link_topico = process.env.NODE_ENV === 'production' && f.path ? f.path : '/uploads/documents/' + f.filename;
+            }
         }
         await Organ.create(data);
         res.redirect('/admin/paginas/organos?saved=1');
@@ -1343,9 +1352,18 @@ controller.updateOrgano = async (req, res) => {
         if (!organ) return res.redirect('/admin/paginas/organos');
         const data = { name: req.body.name, description: req.body.description, color: req.body.color, topic: req.body.topic };
         if (req.files) {
-            if (req.files['reglamento']?.[0])    data.link_reglamento = '/uploads/documents/' + req.files['reglamento'][0].filename;
-            if (req.files['archivo_dinamicas']?.[0]) data.link_dinamicas = '/uploads/documents/' + req.files['archivo_dinamicas'][0].filename;
-            if (req.files['archivo_topico']?.[0])    data.link_topico    = '/uploads/documents/' + req.files['archivo_topico'][0].filename;
+            if (req.files['reglamento']?.[0]) {
+                const f = req.files['reglamento'][0];
+                data.link_reglamento = process.env.NODE_ENV === 'production' && f.path ? f.path : '/uploads/documents/' + f.filename;
+            }
+            if (req.files['archivo_dinamicas']?.[0]) {
+                const f = req.files['archivo_dinamicas'][0];
+                data.link_dinamicas = process.env.NODE_ENV === 'production' && f.path ? f.path : '/uploads/documents/' + f.filename;
+            }
+            if (req.files['archivo_topico']?.[0]) {
+                const f = req.files['archivo_topico'][0];
+                data.link_topico = process.env.NODE_ENV === 'production' && f.path ? f.path : '/uploads/documents/' + f.filename;
+            }
         }
         await organ.update(data);
         res.redirect('/admin/paginas/organos?saved=1');
